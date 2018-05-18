64fce6b42ae5f1541a86cd2d8c547de4b5b11b1d
Aleksandra Jagodzinska <a.jagodzinska@synthrone.com>
2018-05-18 17:00:02 +0200

    Controller added

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-var app = angular.module('store',[])
+(function(){
+
+	var app = angular.module('gemStore',[]);
+
+	app.controller('StoreController', function(){
+		this.product = gems;
+	});
+	var gems = [
+	{
+		name: "Dodeca",
+		price: 2.95,
+		description: "Loremipsum",
+		canPurchase: true,
+	},
+	{
+		name: "Modeca",
+		price: 7.95,
+		description: "Loremipsum",
+		canPurchase: falsethis,
+	}
+	]
+})
